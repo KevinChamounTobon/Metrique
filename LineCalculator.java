df28094692e24379240dc4a7af7a5d5b13672f81
Christian <chrishamaoui@hotmail.com>
2020-10-02 18:34:15 -0400

    Addes visitor classes

--- LineCalculator.java
+++ LineCalculator.java
@@ -1,5 +1,11 @@
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.body.EnumDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -57,5 +63,44 @@ public class LineCalculator {
         int count = 0;
         //Create calculator object
         LineCalculator calc = new LineCalculator("/home/unknown/Documents/projects/Flappy Ghost/src/Coin.java");
+        //Create visitor for classes
+        VoidVisitor<?> classLinesVisitor = new ClassesLineCounter();
+
+        //Call visit method for classes
+        classLinesVisitor.visit(calc.getCu(), null);
+    }
+
+    //Implementation of the line visitor for classes
+    private static class ClassesLineCounter extends VoidVisitorAdapter<Void> {
+
+        public int class_LOC(String[] lines) {
+            return LineCalculator.LOC(lines);
+        }
+
+        public int class_CLOC(String[] lines) {
+            return LineCalculator.CLOC(lines);
+        }
+
+        public void class_DC(String[] lines) {
+            System.out.println("DC: " + (float)class_CLOC(lines) / class_LOC(lines));
+        }
+
+        //Visit the classes or interfaces declaration
+        @Override
+        public void visit(ClassOrInterfaceDeclaration cd, Void arg) {
+            super.visit(cd, arg);
+            String[] lines = cd.toString().split("\n");
+            System.out.println("-----" + cd.getName() + "-----");
+            class_DC(lines);
+        }
+
+        //Visit the enumerators declarations
+        @Override
+        public void visit(EnumDeclaration ed, Void arg) {
+            super.visit(ed, arg);
+            String[] lines = ed.toString().split("\n");
+            System.out.println("-----" + ed.getName() + "-----");
+            class_DC(lines);
+        }
     }
 }
